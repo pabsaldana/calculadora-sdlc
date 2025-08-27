c5379f641f176f1a209834fe0c7e8f5294703796
pabsaldana <164245254+pabsaldana@users.noreply.github.com>
2025-08-27 14:03:47 -0400

    Inicoo del proyecto
    Base para trabajar

---
+++ src/calculadora.py
@@ -0,0 +1,59 @@
+def menu():
+    print("Welcome to my calculator")
+    print("1) Suma")
+    print("2) Resta")
+    print("3) Division")
+    print("4) Multiplicacion")
+    print("5) Elevar")
+    print("6) raiz")
+    print("7) Tabla de multiplicar")
+    print("8) Salir")
+    opcion= int(input("Ingrese opcion a trabajar"))
+    if opcion==1:
+        print()
+        sumar()
+    elif opcion==2:
+        print()
+        restar()
+    elif opcion==3:
+        print()
+        multiplicar()
+    elif opcion==4:
+        print()
+        dividir()
+    elif opcion==5:
+        print()
+        elevar()
+    elif opcion==6:
+        print()
+        raiz_cuadrada()
+    elif opcion==7:
+        print()
+        tablas_multiplicar()
+    elif opcion==8:
+        print()
+    else:
+        print("Opcion no encontrada")
+
+def sumar(numero1, numero2):
+    print()
+    return
+def restar(numero1, numero2):
+    print()
+    return
+def dividir(numero1, numero2):
+    print()
+    return
+def multiplicar(numero1, numero2):
+    print()
+    return
+def elevar(numero1, numero2):
+    print()
+    return
+def raiz_cuadrada(numero1, numero2):
+    print()
+    return
+def tablas_multiplicar(numero1, numero2):
+    print()
+    return
+
